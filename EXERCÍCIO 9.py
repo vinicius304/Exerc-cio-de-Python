ad06796b76e4fa1b683f0841406ed4955eccb0a2
vinicius304 <schaefervinicius912@gmail.com>
2025-10-28 20:55:53 -0300

    Create EXERCÍCIO 9.py

---
+++ EXERCÍCIO 9.py
@@ -0,0 +1,12 @@
+
+
+
+consumo_total = 0
+
+
+for i in range(1, 6):
+    consumo = float(input(f"Digite o consumo da máquina {i} (em kWh): "))
+    consumo_total += consumo  
+
+
+print(f"\nO consumo total da fábrica é: {consumo_total:.2f} kWh")
